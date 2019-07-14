43c5bd6d70ad54085778737f12a078a96daa2271
Dennis <jhbang8917@gmail.com>
2019-07-14 11:25:37 +0900

    commit

--- customer-system/src/main/dennis/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package src.main.dennis;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
-}
---
+++ customer-system/src/main/dennis/UserRepositoryInterface.java
@@ -0,0 +1,4 @@
+package src.main.dennis;
+
+public interface UserRepositoryInterface {
+}

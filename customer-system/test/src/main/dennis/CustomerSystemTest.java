4a891c9551e0d9254d643e8499f1f0467e00bc69
Dennis <jhbang8917@gmail.com>
2019-07-22 21:24:03 +0900

    add junittest file

---
+++ customer-system/test/src/main/dennis/CustomerSystemTest.java
@@ -0,0 +1,35 @@
+package test.src.main.dennis; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* CustomerSystem Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Jul 22, 2019</pre> 
+* @version 1.0 
+*/ 
+public class CustomerSystemTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: findUserById(String id) 
+* 
+*/ 
+@Test
+public void testFindUserById() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
